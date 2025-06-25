255d5c55f6029b133a2464198acbc75d9e018e8f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-06-25 19:26:08 +0200

    added one more test

---
+++ s2gos-client/src/tests/cli/test_cli.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2025 by ESA DTE-S2GOS team and contributors
+#  Permissions are hereby granted under the terms of the Apache 2.0 License:
+#  https://opensource.org/license/apache-2-0.
+
+from unittest import TestCase
+
+from s2gos_client.cli.cli import cli
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+class CliTest(TestCase):
+    def test_help(self):
+        result = runner.invoke(cli, ["--help"])
+        self.assertEqual(0, result.exit_code)
+        self.assertIn("Client tool for the ESA synthetic", result.output)
