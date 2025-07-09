58bc92b76b6e4ae8d107bd6a4ad689c1ec15576e
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-07-09 16:30:07 +0200

    It works!

---
+++ s2gos-airflow/dags/example_dag.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+
+def hello():
+    print("Hello from Airflow!")
+
+with DAG(
+    "S2GOS-Example-DAG",
+    start_date=datetime(2025, 1, 1),
+    schedule=timedelta(days=1),
+    catchup=False,
+    tags=["example"]
+) as dag:
+    task = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello
+    )
