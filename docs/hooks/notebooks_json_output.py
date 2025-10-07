0fc6fb8168abb34081ca6cee35215bf5a98c375c
clarasb <clara.backens@web.de>
2025-10-07 10:18:15 +0200

    reformat hook

--- docs/hooks/notebooks_json_output.py
+++ docs/hooks/notebooks_json_output.py
@@ -1,9 +1,45 @@
-import json, html
-from pathlib import Path
+import html
+import json
 import shutil
+from pathlib import Path
+
 import nbformat as nbf
 
+
+
+def on_pre_build(config):
+    """
+    Runs the updates once before the build of the documentation
+
+    References:
+    - mkdocs hooks: https://www.mkdocs.org/user-guide/configuration/?#hooks
+    - mkdocs events: https://www.mkdocs.org/dev-guide/plugins/#on_pre_build
+
+    Args:
+     - config: global MkDocsConfig Object
+    """
+    source = Path.cwd() / "notebooks"
+    destination = Path.cwd() / "docs/notebooks"
+
+    _update_files_in_docs(source, destination)
+    print(f"[hooks] Updated notebooks: {source} to {destination}")
+
+    for ipynb in destination.rglob("*.ipynb"):
+        if _patch_notebook(ipynb):
+            print(f"[hooks] Patched: {ipynb}")
+
 def _patch_notebook(path: Path):
+    """
+    Changes the output type of Jupyter notebook cells from
+    `json/application` to `text/html` to make the output readable
+    in mkdocs documentation
+
+    Args:
+        - path: Path to notebooks in docs/
+
+    Returns:
+        - boolean indicator to report if notebook patching was done
+    """
     nb = nbf.read(path, as_version=4)
     changed = False
     for cell in nb.cells:
@@ -20,18 +56,12 @@ def _patch_notebook(path: Path):
     return changed
 
 def _update_files_in_docs(source: Path, destination: Path):
+    """
+    Adds notebooks to docs/notebooks
+
+    Args:
+        - source: Path to original notebooks
+        - destination: Path to copy original notebooks to prepare for and add to
+        mkdocs documentation
+    """
     shutil.copytree(source, destination, dirs_exist_ok=True)
-
-# This hook (1) updates the notebooks with each `mkdocs serve`
-# or `mkdocs build` and (2) changes the output type of notebook cells from
-# `json/application` to `text/html` to make the output readable.
-def on_pre_build(config, **kwargs):
-    source = Path.cwd() / "notebooks"
-    destination = Path.cwd() / "docs/notebooks"
-
-    _update_files_in_docs(source, destination)
-    print(f"[hooks] Updated notebooks: {source} to {destination}")
-
-    for ipynb in destination.rglob("*.ipynb"):
-        if _patch_notebook(ipynb):
-            print(f"[hooks] Patched: {ipynb}")
--- docs/notebooks/README.md
+++ docs/notebooks/README.md
@@ -1,3 +1,3 @@
-Into this folder the notebooks from s2gos-controller/notebooks will be copied 
-for the generation of the documentation. This happens with on each build
+Into this folder the notebooks from s2gos-controller/notebooks will be copied
+for the generation of the documentation. This happens on each build
 with the help of the mkdocs hook `docs/hooks/notebooks_json_output.py`.
