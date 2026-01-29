022ec11914aa03590f8f892ba22fd61b1431a011
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-01-29 14:50:20 +0100

    Added support for PathRef component (not yet used)

--- s2gos-client/src/s2gos_client/gui/pathref.py
+++ s2gos-client/src/s2gos_client/gui/pathref.py
@@ -19,7 +19,7 @@ class PathRefEditor(pn.viewable.Viewer):
     title = param.String(default="")
     tooltip = param.String(default=None, allow_None=True)
 
-    path = param.String(default="")
+    value = param.String(default="")
     cid = param.String(default="")
 
     disabled = param.Boolean(default=False)
@@ -27,36 +27,29 @@ class PathRefEditor(pn.viewable.Viewer):
     def __init__(self, **params):
         super().__init__(**params)
 
-        self._path_input = pn.widgets.TextInput(
-            name="Path",
-            placeholder="Path or URL...",
-            value=self.path,
+        self._value_input = pn.widgets.TextInput(
+            name=f"{self.title} URI",
+            placeholder="URI or rel. path...",
+            value=self.value,
             disabled=self.param.disabled,
+            width=300,
         )
 
         self._cid_input = pn.widgets.TextInput(
-            name="CID",
-            placeholder="CID...",
+            name=f"{self.title} CID (optional)",
+            placeholder="Credentials ID...",
             value=self.cid,
             disabled=self.param.disabled,
+            width=120,
         )
 
         # widget ↔ param (two-way, explicit)
-        self._path_input.link(self, value="path")
+        self._value_input.link(self, value="value")
         self._cid_input.link(self, value="cid")
 
-        header = (
-            pn.pane.Markdown(
-                f"**{self.title}**",
-                tooltip=self.tooltip,
-            )
-            if self.title
-            else None
-        )
-
-        self.view = pn.Column(
-            *([header] if header else []),
-            self._path_input,
+        # TODO: set tooltip (how?)
+        self.view = pn.Row(
+            self._value_input,
             self._cid_input,
         )
 
@@ -74,33 +67,35 @@ class PathRefComponent(Component):
         # noinspection PyTypeChecker
         return self.viewable
 
-    def get_value(self) -> dict[str, Any]:
-        return {
-            "value": self.path_ref_editor.path,
-            "cid": self.path_ref_editor.cid or None,
-        }
+    def get_value(self) -> dict[str, Any] | None:
+        value = self.path_ref_editor.value
+        cid = self.path_ref_editor.cid
+        if not (value and cid):
+            return None
+        return {"value": value, "cid": cid or None}
 
-    def set_value(self, value: dict[str, Any]):
-        self.path_ref_editor.path = value.get("value") or ""
+    def set_value(self, value: dict[str, Any] | None):
+        self.path_ref_editor.value = value.get("value") or ""
         self.path_ref_editor.cid = value.get("cid") or ""
 
     def watch_value(self, callback: Callable[[Any], Any]):
-        self.path_ref_editor.param.watch(callback, ["path", "cid"])
+        self.path_ref_editor.param.watch(callback, ["value", "cid"])
 
 
 class PathRefEditorFactory(ComponentFactoryBase):
     type = "object"
-    format = "path_ref"
+    format = "PathRef"
 
     def create_component(
         self, value: JsonValue, title: str, schema: JsonSchemaDict
     ) -> Component:
-        path_ref: dict[str, Any] = value
+        path_ref: dict[str, Any] | None = value
         return PathRefComponent(
             PathRefEditor(
                 title=title,
-                path=path_ref.get("value") or "",
-                cid=path_ref.get("cid") or "",
+                value=(path_ref and path_ref.get("value")) or "",
+                cid=(path_ref and path_ref.get("cid")) or "",
+                tooltip=schema.get("description"),
             )
         )
 
