a0fba0aa0164a692467fe965d546cbd69a226846
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-09-11 16:52:15 +0200

    make S2GOS client work with new auth as of https://github.com/eo-tools/eozilla/pull/212

--- s2gos-client/src/s2gos_client/api.py
+++ s2gos-client/src/s2gos_client/api.py
@@ -2,13 +2,11 @@
 #  Permissions are hereby granted under the terms of the Apache 2.0 License:
 #  https://opensource.org/license/apache-2-0.
 
-import os
-from importlib.resources import files
 from pathlib import Path
 from typing import Any
 
 from cuiman.api import AsyncClient, Client, ClientConfig, ClientError
-from cuiman.api.auth import login_for_tokens
+from cuiman.api.auth import OAuth2AuthConfig
 from pydantic_settings import SettingsConfigDict
 
 
@@ -20,47 +18,18 @@ class S2GOSConfig(ClientConfig):
     )
 
 
-_CONFIG_BASE = S2GOSConfig(
-    api_url="https://s2gos.wraptile.brockmann-consult.de/",
-    auth_type="login",
-    auth_url=(
-        "https://kc.dev.brockmann-consult.de/realms/dte-s2gos/protocol"
-        "/openid-connect/token"
-    ),
-    client_id="cuiman",
-    grant_type="password",
-    use_bearer=True,
-)
-_DEBUG = False
-
 ClientConfig.default_path = Path("~").expanduser() / ".s2gos-client"
-ClientConfig.default_config = _CONFIG_BASE
-
-# Default show_app() to the S2GOS-branded GUI build bundled with this
-# package, unless the user has already set EOZILLA_APP_DIST themselves
-# (e.g. to point at a local frontend dev build).
-os.environ.setdefault(
-    "EOZILLA_APP_DIST", str(files("s2gos_client.app").joinpath("dist"))
+ClientConfig.default_config = S2GOSConfig(
+    api_url="https://s2gos.wraptile.brockmann-consult.de/",
+    auth=OAuth2AuthConfig(
+        token_url=(
+            "https://kc.dev.brockmann-consult.de/realms/dte-s2gos/protocol"
+            "/openid-connect/token"
+        ),
+        client_id="cuiman",
+        grant_type="password",
+    ),
 )
-
-
-def _create_config(**config_overrides: Any) -> ClientConfig:
-    """
-    Create the S2GOS-specific configuration instance
-    from given configuration overrides.
-    """
-    config = S2GOSConfig.create(**config_overrides)
-    if config.auth_type != "login":
-        return config
-
-    # Always obtain fresh tokens: a token read from persistent configuration is
-    # likely expired, and so is its refresh token. Authentication stays of type
-    # "login", which lets the underlying transport refresh the access token
-    # after a 401 response.
-    result = login_for_tokens(config)
-    config.token = result.access_token
-    config.refresh_token = result.refresh_token
-    return config
 
 
 def create_client(**config: Any) -> Client:
@@ -78,7 +47,7 @@ def create_client(**config: Any) -> Client:
         An instance of a synchronous cuiman client for S2GOS. See
         https://eo-tools.github.io/eozilla/cuiman/ for details.
     """
-    return Client(config=_create_config(**config), _debug=_DEBUG)
+    return Client(**config)
 
 
 def create_async_client(**config: Any) -> AsyncClient:
@@ -96,7 +65,7 @@ def create_async_client(**config: Any) -> AsyncClient:
         An instance of an asynchronous cuiman client for S2GOS. See
         https://eo-tools.github.io/eozilla/cuiman/ for details.
     """
-    return AsyncClient(config=_create_config(**config), _debug=_DEBUG)
+    return AsyncClient(**config)
 
 
 __all__ = [
